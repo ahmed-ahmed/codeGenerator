89240de609a1cfc40569320a028c7b188be2527c
Ahmed M Ahmed <aahmed@cas.org>
2017-04-12 15:26:12 -0400

    global code generation
    - add directory support
    - separate generator project from the code templates
    - use global .gen folder

--- index.js
+++ index.js
@@ -1,66 +1,107 @@
+#!/usr/bin/env node
+
+const generatorsLoacation = `${getUserHome()}/.gen`;
+
+var path = require('path');
 var prompt = require('prompt');
 var fs = require('fs');
 var _ = require('lodash');
+var chalk       = require('chalk');
+var clear       = require('clear');
+var figlet      = require('figlet');
+var mkdirp = require('mkdirp');
 
-var colors = require("colors/safe");
+// get all generators names 
+var gens = fs.readdirSync(generatorsLoacation+ '/');
+//////////////////////////
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Gen', { horizontalLayout: 'full' })
+  )
+);
 
-var generator = require('./template.js').generator;
+// showing all the available generators
+let inquirer = require('inquirer');
+var question = [{
+    type: 'list',
+    name: 'size',
+    message: 'Which generator do you want to use?',
+    choices: gens,
+}];
 
-var generatorPrompets = generator.prompts.map(e=> e.name)
+inquirer.prompt(question).then(function (answers) {
+	var selectedGenerator = `${generatorsLoacation}/${answers.size}`
+	var colors = require("colors/safe");
+	var generator = require(`${selectedGenerator}/index.js`).generator;
+	var generatorPrompets = generator.prompts.map(e=> e.name)
 
-prompt.message = colors.rainbow("Question!");
-prompt.delimiter = colors.green("><");
+	prompt.message = colors.rainbow("Question!");
+	prompt.delimiter = colors.green("><");
 
-prompt.start();
+	prompt.start();
 
-prompt.get(generatorPrompets, function (err, result) {
-	// console.log(result.name);
+	prompt.get(generatorPrompets, function (err, result) {
+		generator.actions.forEach((a)=> {
+			if(a.type === 'add') {
+				try {  
+				    var data = fs.readFileSync(`${selectedGenerator}/${a.template}`, 'utf8');
+				    let destination = formate(a.path, result);
+				    console.log(destination);
+				    let formattedOutput = formate(data,  result);
 
-	generator.actions.forEach((a)=> {
-		if(a.type === 'add') {
-			try {  
-			    var data = fs.readFileSync(a.template, 'utf8');
-			    let destination = formate(a.path, result);
-			    console.log(destination);
-			    let formattedOutput = formate(data,  result);
-			    console.log(formattedOutput);
-			    fs.writeFile(destination, formattedOutput, function(err) {
-				    if(err) {
-				        return console.log(err);
-				    }
-				    console.log("The file was saved!");
-				}); 
+					ensureDirectoryExistence(destination);
+				    // mkdirp.sync(destination)
 
-
-			} catch(e) {
-			    console.log('Error:', e.stack);
+				    fs.writeFileSync(destination, formattedOutput);
+				} catch(e) {
+				    console.log('Error:', e.stack);
+				}
 			}
-		}
-	})
-});
+		})
+	});
+})
 
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
+
+// function mkdirp(filepath) {
+//     var dirname = path.dirname(filepath);
+
+//     if (!fs.existsSync(dirname)) {
+//         mkdirp(dirname);
+//     }
+
+//     fs.mkdirSync(filepath);
+// }
+
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
 
 function formate(input, result) {
 	// console.log(input)
 	let pattern = /<<(\w*)\s*(\w*)>>/gm;
 
 	while (matches = pattern.exec(input)) {
-		console.log(matches);
-		console.log(matches.length);
 		if(matches[2]) {//formatting added
-			console.log(0);
 			let functionName = matches[1];
 			let varName = matches[2];
 			input = input.replace(`${functionName} `, '')
 					.replace(`<<${varName}>>`
 						,'${' +functionName + '(result.' + varName + ')}')
 					// .replace('<<${m[2]}>>', '${result.'+ matches[1]+ '}');
-			console.log(input);
-			// console.log(0);
 
 		} else {
 	 	    input = input.replace(matches[0], '${result.'+ matches[1]+ '}');
-	 	    console.log(1);
 		}
  	}
 
